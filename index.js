f3a91c8394c2232d080566e8208ce41fde325c59
MetaSean <metasean@users.noreply.github.com>
2015-04-28 12:39:46 -0600

    gun.attach(app);  TypeError: Object #<Gun> has no method 'attach'

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 /****************************************************
 	NOT included in the actual "Try it now" steps 
 ****************************************************/
+/* Server Option: Setup an express.js server */
+/* Install express package, e.g.
+	sudo npm install express --save
+	*/
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 80;
+
+var express = require('express');
+var app = express();
 
 
 /* require GunDB in the server js file, e.g.
 	var Gun = require('./gun/gun.js'); 
 	*/
+//                                     ↓↓↓ Running `sudo npm install gun --save`
+//                                         took care of the individual errors below,
+//                                         BUT now all I can get is:
+//                                         TypeError: Object #<Gun> has no method 'attach'	
+//	var Gun = require('gun');          <-- This is what is shown in all the examples
+//                                         but always gives me
+//                                         Error: Cannot find module 'gun'
+//	var Gun = require('./gun');        <-- Error: Cannot find module 'formidable'
+//	var Gun = require('./gun/gun.js'); <-- Was working, until I switched to Express
+//                                         and added gun.attach(app); now sends
+//                                         TypeError: Object #<Gun> has no method 'attach'
 	var Gun = require('./gun/gun.js');
+
 
 /****************************************************
 	Included in the actual "Try it now" steps 
@@ -20,7 +40,7 @@
 		var gun = Gun(location.origin + '/gun'); <-- DIDN'T WORK FOR ME 
 		                                         <-- ReferenceError: location is not defined
 		OR 
-		var gun = Gun({ file: 'data.json' });
+		var gun = Gun({ file: 'data.json' }).key('hello/world/data');
 		OR
 		var gun = Gun({
 			file: 'data.json',
@@ -31,19 +51,19 @@
 			}
 		});
 		*/
-	var gun = Gun({ file: 'data.json' }); 
+	var gun = Gun({ file: 'data.json' }).key('hello/world/data'); 
 
 	/* Save an initial object, e.g.
 		gun.set({hello: "world"}).key('random/0gk7RVviW');
 		OR
 		gun.set({hello: "world!"}).key('hello/world/data');
 		OR
-		gun.set({hello: "world"});
+		gun.set({hello: "world"});  <-- WRONG - NEEDS A KEY
 		*/
-	gun.set({hello: "world!"});
+	gun.set({hello: "world!"}).key('hello/world/data');
 
 /* Try it now - step 2 */
-	/* Load data file in the main html file */
+	/* Load data in the main html file */
 	/* Display stringified data into the main html file's body section */
 
 
@@ -51,15 +71,26 @@
 	NOT included in the actual "Try it now" steps 
 ****************************************************/
 
-/* Setup a basic node.js server  */
-var http = require('http');
-http.createServer(function(req, res){
-  gun.load('my/first/data', function(err, data){
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(data));
-  });
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+/* Server Option: Setup a basic node.js server  
+	var http = require('http');
+	http.createServer(function(req, res){
+	  gun.load('my/first/data', function(err, data){
+	    res.writeHead(200, {'Content-Type': 'application/json'});
+	    res.end(JSON.stringify(data));
+	  });
+	}).listen(1337, '127.0.0.1');
+	console.log('Server running at http://127.0.0.1:1337/');
+	*/
+
+/* Server Option: Setup an express.js server */
+/* Install express package, e.g.
+	sudo npm install express --save
+	*/
+
+gun.attach(app);
+app.use(express.static(__dirname)).listen(port);
+
+console.log('Server started on port ' + port + ' with /gun');
 
 
 
